800f9901195401a29ffd2a981f3cf8aac1c1ae01
Luyao Tian <tianluyao795@gmail.com>
2017-11-10 14:32:47 -0700

    Added cpp file, function TODO

---
+++ Tools/MeshBuilder/cMeshBuilder.cpp
@@ -0,0 +1,23 @@
+// Include Files
+//==============
+
+#include "cMeshBuilder.h"
+
+#include <Tools/AssetBuildLibrary/Functions.h>
+#include <utility>
+#include <Engine/Platform/Platform.h>
+
+// Inherited Implementation
+//=========================
+
+// Build
+//------
+
+eae6320::cResult eae6320::Assets::cMeshBuilder::Build(const std::vector<std::string>&)
+{
+	auto result = eae6320::Results::Success;
+
+
+
+	return result;
+}
