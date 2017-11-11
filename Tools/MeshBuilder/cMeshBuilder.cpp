83a584f2c1b2d83e1fe63d5d02f070a194450175
Luyao Tian <tianluyao795@gmail.com>
2017-11-10 17:02:50 -0700

    Modified error message

--- Tools/MeshBuilder/cMeshBuilder.cpp
+++ Tools/MeshBuilder/cMeshBuilder.cpp
@@ -6,6 +6,25 @@
 #include <Tools/AssetBuildLibrary/Functions.h>
 #include <Engine/Platform/Platform.h>
 #include <Engine/Asserts/Asserts.h>
+#include <Engine/Results/Results.h>
+#include <External/Lua/Includes.h>
+#include <iostream>
+
+// Helper Function Declarations
+//=============================
+
+namespace
+{
+	eae6320::cResult LoadTableValues(lua_State& io_luaState);
+
+	eae6320::cResult LoadTableValues_vertices(lua_State& io_luaState);
+	eae6320::cResult LoadTableValues_vertices_values(lua_State& io_luaState);
+
+	eae6320::cResult LoadTableValues_indices(lua_State& io_luaState);
+	eae6320::cResult LoadTableValues_indices_values(lua_State& io_luaState);
+
+	eae6320::cResult LoadAsset(const char* const i_path);
+}
 
 // Inherited Implementation
 //=========================
@@ -17,10 +36,15 @@ eae6320::cResult eae6320::Assets::cMeshBuilder::Build(const std::vector<std::str
 {
 	auto result = eae6320::Results::Success;
 
-	if (!(result = eae6320::Platform::CopyFileA(m_path_source, m_path_target, false, true)))
+	std::string * errorMessage;
+
+	if (!(result = eae6320::Platform::CopyFileA(m_path_source, m_path_target, false, true, errorMessage)))
 	{
-		OutputErrorMessageWithFileInfo(m_path_source, "Failed to copy file to destination");
+		OutputErrorMessageWithFileInfo(m_path_source, errorMessage->c_str());
 	}
 
 	return result;
 }
+
+// Helper Function Definitions
+//============================
