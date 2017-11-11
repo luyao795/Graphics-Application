90a550ab7475c8f14e4d08ae083afa2ac31dad23
Luyao Tian <tianluyao795@gmail.com>
2017-11-10 17:18:38 -0700

    Added LoadAsset function

--- Tools/MeshBuilder/cMeshBuilder.cpp
+++ Tools/MeshBuilder/cMeshBuilder.cpp
@@ -48,3 +48,101 @@ eae6320::cResult eae6320::Assets::cMeshBuilder::Build(const std::vector<std::str
 
 // Helper Function Definitions
 //============================
+
+eae6320::cResult LoadAsset(const char* const i_path)
+{
+	auto result = eae6320::Results::Success;
+
+	// Create a new Lua state
+	lua_State* luaState = nullptr;
+	{
+		luaState = luaL_newstate();
+		if (!luaState)
+		{
+			result = eae6320::Results::OutOfMemory;
+			std::cerr << "Failed to create a new Lua state" << std::endl;
+			goto OnExit;
+		}
+	}
+	// Execute the "chunk", which should load the asset
+	// into a table at the top of the stack
+	// Load the asset file as a "chunk",
+	// meaning there will be a callable function at the top of the stack
+	const auto stackTopBeforeLoad = lua_gettop(luaState);
+	{
+		const auto luaResult = luaL_loadfile(luaState, i_path);
+		if (luaResult != LUA_OK)
+		{
+			result = eae6320::Results::Failure;
+			std::cerr << lua_tostring(luaState, -1) << std::endl;
+			// Pop the error message
+			lua_pop(luaState, 1);
+			goto OnExit;
+		}
+	}
+	// Execute the "chunk", which should load the asset
+	// into a table at the top of the stack
+	{
+		constexpr int argumentCount = 0;
+		constexpr int returnValueCount = LUA_MULTRET;	// Return _everything_ that the file returns
+		constexpr int noMessageHandler = 0;
+		const auto luaResult = lua_pcall(luaState, argumentCount, returnValueCount, noMessageHandler);
+		if (luaResult == LUA_OK)
+		{
+			// A well-behaved asset file will only return a single value
+			const auto returnedValueCount = lua_gettop(luaState) - stackTopBeforeLoad;
+			if (returnedValueCount == 1)
+			{
+				// A correct asset file _must_ return a table
+				if (!lua_istable(luaState, -1))
+				{
+					result = eae6320::Results::InvalidFile;
+					std::cerr << "Asset files must return a table (instead of a " <<
+						luaL_typename(luaState, -1) << ")" << std::endl;
+					// Pop the returned non-table value
+					lua_pop(luaState, 1);
+					goto OnExit;
+				}
+			}
+			else
+			{
+				result = eae6320::Results::InvalidFile;
+				std::cerr << "Asset files must return a single table (instead of " <<
+					returnedValueCount << " values)" << std::endl;
+				// Pop every value that was returned
+				lua_pop(luaState, returnedValueCount);
+				goto OnExit;
+			}
+		}
+		else
+		{
+			result = eae6320::Results::InvalidFile;
+			std::cerr << lua_tostring(luaState, -1) << std::endl;
+			// Pop the error message
+			lua_pop(luaState, 1);
+			goto OnExit;
+		}
+	}
+
+	// If this code is reached the asset file was loaded successfully,
+	// and its table is now at index -1
+	result = LoadTableValues(*luaState);
+
+	// Pop the table
+	lua_pop(luaState, 1);
+
+OnExit:
+
+	if (luaState)
+	{
+		// If I haven't made any mistakes
+		// there shouldn't be anything on the stack,
+		// regardless of any errors encountered while loading the file:
+		EAE6320_ASSERT(lua_gettop(luaState) == 0);
+
+		lua_close(luaState);
+		luaState = nullptr;
+	}
+
+	return result;
+}
